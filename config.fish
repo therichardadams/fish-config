f972efdcffa3fb3e1db2338db06c00955fc3ec7c
Richard Adams <dev@radams.io>
2025-04-01 14:32:37 +0100

    Add path and aliases

--- config.fish
+++ config.fish
@@ -5,3 +5,15 @@ set -U fish_greeting
 eval "$(/opt/homebrew/bin/brew shellenv)"
 set -gx VISUAL nvim
 set -gx EDITOR nvim
+fish_add_path -P /usr/local/bin /usr/local/sbin
+
+abbr --add cs cd ~/Source
+abbr --add t tldr
+abbr --add v nvim
+abbr --add v. nvim .
+abbr --add vim nvim
+abbr --add py python3
+abbr --add epp export PYTHONPATH=`pwd`
+abbr --add sva source venv/bin/activate.fish
+
+loadenv ~/.keys.env
